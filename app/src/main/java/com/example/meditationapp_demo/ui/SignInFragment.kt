3f1441540f4d618b81ea80190188da7cf5ac12cf
GokceKahraman <gokcekahraman0@gmail.com>
2021-08-05 13:45:20 +0300

    .

--- app/src/main/java/com/example/meditationapp_demo/ui/SignInFragment.kt
+++ app/src/main/java/com/example/meditationapp_demo/ui/SignInFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import com.example.meditationapp_demo.R
+import com.google.android.material.textfield.TextInputEditText
 
 class SignInFragment : Fragment() {
+
+    lateinit var signIn_textView : ImageView
+    lateinit var sign_Username : TextInputEditText
+    lateinit var sign_Password : TextInputEditText
+    lateinit var signIn_button : Button
 
 
     override fun onCreateView(
@@ -16,6 +24,11 @@ class SignInFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
+
+        signIn_button.setOnClickListener{
+        }
+
+
     }
 
 
