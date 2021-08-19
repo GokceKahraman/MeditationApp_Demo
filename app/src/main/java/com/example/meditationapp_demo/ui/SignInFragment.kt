ce46032c8a0c46e80fad7f1089b2a652919f7950
GokceKahraman <gokcekahraman0@gmail.com>
2021-08-19 09:51:39 +0300

    .

--- app/src/main/java/com/example/meditationapp_demo/ui/SignInFragment.kt
+++ app/src/main/java/com/example/meditationapp_demo/ui/SignInFragment.kt
@@ -28,11 +28,7 @@ class SignInFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
-
-
-       //sharedPreferences = this@SignInFragment.get
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
