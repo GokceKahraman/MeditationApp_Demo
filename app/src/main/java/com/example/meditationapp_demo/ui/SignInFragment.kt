5e1cae5ad2816e45b4fd810c3c778ff4c68a6c9a
GokceKahraman <gokcekahraman0@gmail.com>
2021-08-30 15:40:42 +0300

    .

--- app/src/main/java/com/example/meditationapp_demo/ui/SignInFragment.kt
+++ app/src/main/java/com/example/meditationapp_demo/ui/SignInFragment.kt
@@ -1,57 +1,53 @@
 package com.example.meditationapp_demo.ui
-import android.content.Intent
-import android.content.SharedPreferences
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import androidx.appcompat.app.AlertDialog
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.meditationapp_demo.R
 import com.example.meditationapp_demo.databinding.FragmentSignInBinding
-import com.google.android.material.textfield.TextInputEditText
 
 class SignInFragment : Fragment() {
-    lateinit var signIn_textView : ImageView
-    lateinit var sign_Username : TextInputEditText
-    lateinit var sign_Password : TextInputEditText
-    lateinit var signIn_button : Button
-    lateinit var sharedPreferences: SharedPreferences
     private lateinit var binding: FragmentSignInBinding
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentSignInBinding.inflate(inflater,container,false)
-
-
-
+    ): View {
+        binding = FragmentSignInBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        if (sign_Username.length()<2){
-
-        }
-
         binding.signInButton.setOnClickListener {
-            val direction = SignInFragmentDirections.actionSignInFragmentToHomeFragment()
-            findNavController().navigate(direction)
-
-
+            if (signInTextViewControl()){
+                val direction = SignInFragmentDirections.actionSignInFragmentToHomeFragment()
+                findNavController().navigate(direction)
+            }
 
         }
     }
 
+    fun signInTextViewControl() : Boolean{
+        // username includes at least 2 characters, 1 upper, 1 small
+        // password includes at least 1 upper, 1 small 1 special character
+        if (binding.signUserName.length() <= 2) {
+            binding.signUserName.setError("Username must be longer than 2 characters")
+        } else if (binding.signPassword.length() < 6 || !"([A-Za-z0-9]+)".toRegex()
+                .matches(binding.signPassword.text.toString())
+        ) {
+            binding.signPassword.setError("Password should consist minimum 6 characters with at least 1 uppercase, 1 number")
+        }
+        else {
+            binding.signUserName.setError(null)
+            binding.signPassword.setError(null)
+            return true
+        }
+        return false
+    }
 
 
 }
